b79feacf5a0d157b79d710f9415a6a6309845f9a
fengcun1993 <fengcun1993@vip.qq.com>
2017-09-27 17:41:02 +0800

    sd

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1506412015,
+	"version": 1506505190,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -9,15 +9,12 @@
 		"images/sanastick-sheet0.png",
 		"images/sanapad-sheet0.png",
 		"images/plane-sheet0.png",
-		"images/plane-sheet1.png",
-		"images/plane-sheet2.png",
 		"images/background-sheet0.png",
 		"images/bullet-sheet0.png",
 		"images/tail-sheet0.png",
 		"images/tail-sheet1.png",
 		"images/explode-sheet0.png",
 		"images/explode-sheet1.png",
-		"images/plane2-sheet0.png",
 		"images/explode2-sheet0.png",
 		"images/throw-sheet0.png",
 		"images/grenade-sheet0.png",
